bcf4215d3f07d7c24556211fd690b94ea3078fed
mario <mariojuegosydemas@gmail.com>
2025-09-16 11:49:19 +0200

    Add files via upload

---
+++ suma.py
@@ -0,0 +1,8 @@
+def programa1():
+    #cadena=raw_input("dime tu nombre princesa: ")
+    #print("bienvenida "+ cadena)
+    num1=input("dime un numero: ")
+    num2=input("dime otro numero: ")
+    print(str(num1)+"+"+str(num2)+"="+str(num1+num2)) #str conviete numero a cadena
+
+programa1()
